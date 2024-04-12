e10f83cf05b6e83b7fed40d254a3480351e73ad5
Danis <dandijar@yandex.ru>
2024-04-12 12:44:32 +0500

    if this works i will destroy the whole internet

--- db/session.py
+++ db/session.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 engine = create_async_engine(settings.POSTGRES_DATABASE_URL, echo=True)  # SQLALCHEMY_DATABASE_URL
-SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+SessionLocal = sessionmaker(expire_on_commit=False, class_=AsyncSession, bind=engine)
 # sync_engine = create_engine(settings.POSTGRES_DATABASE_URL, echo=True)  # SYNC_SQLALCHEMY_DATABASE_URL
 # SyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sync_engine)
