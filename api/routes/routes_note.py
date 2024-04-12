185db6958d0f860ae540dcee7dedc67eadac6f16
Danis <dandijar@yandex.ru>
2024-04-12 13:32:05 +0500

    return

--- api/routes/routes_note.py
+++ api/routes/routes_note.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import APIRouter, HTTPException, Depends
-from api.deps import CurrentUser, get_db
+from fastapi import APIRouter, HTTPException
+
+from api.deps import CurrentUser, DBSession
 from schema.note_schema import SchemaNote, SchemaNoteList, SchemaNoteDB, SchemaNoteOptional
 from crud import crud_note
 
@@ -8,13 +9,13 @@ router = APIRouter()
 
 
 @router.get("/notes/", response_model=SchemaNoteList)
-async def get_notes(user_id: Optional[int], db= Depends(get_db)) -> SchemaNoteList:
+async def get_notes(db: DBSession, user_id: Optional[int]) -> SchemaNoteList:
     notes = await crud_note.get_note_list(db, user_id)
     return SchemaNoteList(notes=notes)
 
 
 @router.get("/notes/{note_id}", response_model=SchemaNote)
-async def get_note(note_id: int, db= Depends(get_db)):
+async def get_note(db: DBSession, note_id: int):
     note = await crud_note.get_note(db, note_id)
     if note:
         return note
@@ -23,13 +24,13 @@ async def get_note(note_id: int, db= Depends(get_db)):
 
 
 @router.post('/notes/', response_model=SchemaNoteDB)
-async def create_note(note: SchemaNote, current_user: CurrentUser, db= Depends(get_db)):
+async def create_note(db: DBSession, note: SchemaNote, current_user: CurrentUser):
     created_note = await crud_note.create_note(db, note, current_user)
     return created_note
 
 
 @router.put('/notes/{note_id}', response_model=SchemaNote)
-async def change_note(note_id: int, note: SchemaNoteOptional, db= Depends(get_db)):
+async def change_note(db: DBSession, note_id: int, note: SchemaNoteOptional):
     changed_note = await crud_note.change_note(db, note_id, note)
     if not changed_note:
         raise HTTPException(404, f"Не найдена запись Note с id={note_id}")
@@ -37,5 +38,5 @@ async def change_note(note_id: int, note: SchemaNoteOptional, db= Depends(get_db
 
 
 @router.delete('/notes/{note_id}')
-async def delete_note(note_id: int, db= Depends(get_db)):
+async def delete_note(db: DBSession, note_id: int):
     await crud_note.delete_note(db, note_id)
--- crud/crud_user.py
+++ crud/crud_user.py
@@ -9,10 +9,10 @@ from schema.user_schema import SchemaUser, SchemaUserOptional, SchemaUserWithPas
 from utils.password_utils import get_hashed_password
 
 
-async def get_user_by_id(db, user_id: int) -> Optional[User]:
+async def get_user_by_id(db: AsyncSession, user_id: int) -> Optional[User]:
     """
     CRUD async GET that returns the user with given id
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param user_id: a required parameter that identifies the user
     :return: found User object
     """
@@ -21,10 +21,10 @@ async def get_user_by_id(db, user_id: int) -> Optional[User]:
     return result.scalars().first()
 
 
-async def get_user_by_login(db, login: str) -> Optional[User]:
+async def get_user_by_login(db: AsyncSession, login: str) -> Optional[User]:
     """
     CRUD async GET that returns the user with given id
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param login: a required parameter that identifies the user
     :return: found User object
     """
@@ -33,10 +33,10 @@ async def get_user_by_login(db, login: str) -> Optional[User]:
     return result.scalars().first()
 
 
-async def create_user(db, user: SchemaUserWithPassword) -> Optional[User]:
+async def create_user(db: AsyncSession, user: SchemaUserWithPassword) -> Optional[User]:
     """
     CRUD async POST that creates and returns the user
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param user: schema that contains the data
     :return: created User object or None if there's already a user with the same login
     """
@@ -48,10 +48,10 @@ async def create_user(db, user: SchemaUserWithPassword) -> Optional[User]:
         return new_user
 
 
-async def change_user(db, user_id: int, user: SchemaUserOptional) -> Optional[User]:
+async def change_user(db: AsyncSession, user_id: int, user: SchemaUserOptional) -> Optional[User]:
     """
     CRUD async POST that creates and returns the user
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param user_id: a required parameter that identifies the user
     :param user:  schema that contains the data (which is optional to give)
     :return: changed User object or None if not found
@@ -66,10 +66,10 @@ async def change_user(db, user_id: int, user: SchemaUserOptional) -> Optional[Us
     return put_result.scalars().first()
 
 
-async def delete_user(db, user_id: int) -> None:
+async def delete_user(db: AsyncSession, user_id: int) -> None:
     """
     CRUD async DELETE that deletes a User with given note_id
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param user_id: required parameter that identifies a User
     :return: None
     """
@@ -78,10 +78,10 @@ async def delete_user(db, user_id: int) -> None:
     await db.commit()
 
 
-async def get_user_by_token(db, token: str) -> Optional[User]:
+async def get_user_by_token(db: AsyncSession, token: str) -> Optional[User]:
     """
     Async function that uses a token to get current user
-    :param db that executes the statement
+    :param db: AsyncSession that executes the statement
     :param token: a JWT that defines the searched user
     :return: found User or None if it couldn't be found
     """
